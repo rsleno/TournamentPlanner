d32d77e72acfab7976f698859c9b181eca71c7e0
Roberto Sanz <rsleno@gmail.com>
2015-04-26 10:27:59 +0200

    Adding views

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,6 +6,10 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE DATABASE tournament;
+
+\c tournament
+
 CREATE TABLE IF NOT EXISTS players (
 	id serial PRIMARY KEY,
 	name text 
@@ -14,6 +18,17 @@ CREATE TABLE IF NOT EXISTS players (
 CREATE TABLE IF NOT EXISTS matches (
 	id serial PRIMARY KEY,
 	winner integer REFERENCES players (id),
-	looser integer REFERENCES players (id)
+	loser integer REFERENCES players (id)
 );
+
+CREATE VIEW total_matches AS
+	SELECT players.id, count(*) AS num FROM players 
+	JOIN matches ON players.id = matches.winner OR players.id = matches.loser
+	GROUP BY players.id
+;
+
+CREATE VIEW total_wins AS
+	SELECT players.id, count(matches.winner) AS wins FROM players 
+	JOIN matches ON players.id = matches.winner GROUP BY players.id
+;
 
