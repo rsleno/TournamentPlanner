6e24392a4e449f19a35264e0421b58dc78f468c9
rsanz <roberto.sanz@ticketbis.com>
2015-05-09 15:06:22 +0200

    Adding edit, delete

--- vagrant/catalog/project.py
+++ vagrant/catalog/project.py
@@ -1,10 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+# Create session and connect db
+engine = create_engine('sqlite:///restaurantMenu.db')
+Base.metadata.bind=engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-	return "Hello World"
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+	restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+	items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id).all()
+	return render_template('menu.html', restaurant = restaurant, items = items)
+
+@app.route('/restaurants/<int:restaurant_id>/new/')
+def newMenuItem(restaurant_id):
+	return "page to create a new item in the menu"
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
+def editMenuItem(restaurant_id, menu_id):
+	return "page to edit item from menu"
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/')
+def deleteMenuItem(restaurant_id, menu_id):
+	return "page to delete item from menu"
 
 if __name__ == '__main__':
 	app.debug = True
